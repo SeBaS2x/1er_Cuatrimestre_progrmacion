844c24657f15805bd396227f4f7ff691860e84c0
SeBaS2x <sebastiancamacho2311@gmail.com>
2025-05-26 17:07:05 -0300

    para continuar

--- clase_8/desafio1_array_menu.py
+++ clase_8/desafio1_array_menu.py
@@ -1,1 +1,10 @@
-a
+contador = 0
+
+
+
+
+while contador < 10:
+    numeros = (int(input("ingrese un numero entero :")))
+    if numeros < -1000 and numeros > 1000:
+        contador +=1 
+        
--- clase_8/modulo_funciones_arrays_clase8.py
+++ clase_8/modulo_funciones_arrays_clase8.py
@@ -10,13 +10,35 @@ def mostrar_vector(vector):
     for i in range(len(vector)):
         print(vector[i])
         
-######MAIN#############
 def buscar_minimo(vector:list):
     for i in range(len(vector)):
         if i == 0 or vector[i]<minimo:
             minimo = vector[i]
     return minimo
 
+def mostrar_posiciones_valor(vector, valor):
+    bandera = False
+    for i in range(len(vector)):
+        if vector[i] == valor:
+            print(i)
+            bandera = True
+    return bandera
+
+
+def verificar_paridad(numero):
+    es_par = True
+    if numero % 2 != 0:
+        es_par = False
+    return es_par
+
+def indicar_si_hay_impar(vector):
+    encontro = False
+    for i in range(len(vector)):
+        if verificar_paridad(vector[i]) == False:
+            encontro = True
+    return encontro
+    
+    
 crear =crear_vector(0,5)
 cargar_vector(crear, "ingresar numero del vector ")
 mostrar_vector(crear)
